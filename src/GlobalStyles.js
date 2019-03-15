7651fb5757394011f2ea3da21a79576a9a7d703d
Miguel Castrillon <miguel.chory@hotmail.com>
2019-03-14 21:51:29 -0500

    Fix GlobalStyles with StyledComponents Add logo to Menu component Upgrade dependencies

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -3,6 +3,10 @@ import { createGlobalStyle } from 'styled-components'
 export default createGlobalStyle`
   @import url("https://fonts.googleapis.com/css?family=Lato:300,400,700");
   
+  html, body {
+      height: 100%;
+  }
+  
   body, *, *:before, *:after {
       font-family: 'Lato', sans-serif;
       font-weight: 400;
@@ -13,5 +17,9 @@ export default createGlobalStyle`
   body {
       padding: 0;
       margin: 0;
+  }
+  
+  #__next {
+      height: 100%
   }
 `
